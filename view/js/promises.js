1c47f6d0a57c817f7e4edc4eda788f4f9bb77ffe
alex <alex.mart.juan@gmail.com>
2024-05-09 10:06:12 +0200

    AllModule_Implementación_CodigoNecesario_PrettyURL

--- view/js/promises.js
+++ view/js/promises.js
@@ -14,6 +14,24 @@ function ajaxPromise(sUrl, sType, sTData, sData = undefined) {
 };
 // function ajaxPromise(sType, sTData, sUrl, sData = undefined)
 
+/* FRIENDLY URL */
+function friendlyURL(url) {
+    var link = "";
+    url = url.replace("?", "");
+    url = url.split("&");
+    cont = 0;
+    for (var i = 0; i < url.length; i++) {
+    	cont++;
+        var aux = url[i].split("=");
+        if (cont == 2) {
+        	link += "/" + aux[1] + "/";	
+        }else{
+        	link += "/" + aux[1];
+        }
+    }
+    return "http://localhost/Framework/HousingProject_Framework" + link;
+}
+
 //================LOAD-HEADER================
 function load_menu() {
     $('<li></li>').html('<a href="?module=home&op=view" class="active">Home</a>').appendTo('#navbar ul');
