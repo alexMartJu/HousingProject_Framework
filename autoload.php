3e062b844a883efc47e5f13453282193e2f468f4
alex <alex.mart.juan@gmail.com>
2024-05-10 22:52:05 +0200

    AllModule_Autoload_por_Modulos

--- autoload.php
+++ autoload.php
@@ -4,7 +4,7 @@
     
     spl_autoload_extensions('.php,.inc.php,.class.php,.class.singleton.php');
     spl_autoload_register('loadClasses');
-    
+
     function loadClasses($className) {
         $breakClass = explode('_', $className);
         $modelName = "";
@@ -12,11 +12,32 @@
         if (isset($breakClass[1])) {
             $modelName = strtoupper($breakClass[1]);
         }
-        
-        if (file_exists(SITE_ROOT . 'module/' . $breakClass[0] . '/model/'. $modelName . '/' . $className . '.class.singleton.php')) {
-            set_include_path('module/' . $breakClass[0] . '/model/' . $modelName.'/');
-            spl_autoload($className);
-        }else if (file_exists(SITE_ROOT . 'model/' . $className . '.class.singleton.php')){
+
+        if ($breakClass[0] === 'home') {
+            if (file_exists(SITE_ROOT . 'module/' . $breakClass[0] . '/model/'. $modelName . '/' . $className . '.class.singleton.php')) {
+                // error_log("Entro home");
+                set_include_path('module/' . $breakClass[0] . '/model/' . $modelName.'/');
+                spl_autoload($className);
+            }
+        } else if ($breakClass[0] === 'shop') {
+            if (file_exists(SITE_ROOT . 'module/' . $breakClass[0] . '/model/'. $modelName . '/' . $className . '.class.singleton.php')) {
+                // error_log("Entro shop");
+                set_include_path('module/' . $breakClass[0] . '/model/' . $modelName.'/');
+                spl_autoload($className);
+            }
+        } else if ($breakClass[0] === 'search') {
+            if (file_exists(SITE_ROOT . 'module/' . $breakClass[0] . '/model/'. $modelName . '/' . $className . '.class.singleton.php')) {
+                // error_log("Entro search");
+                set_include_path('module/' . $breakClass[0] . '/model/' . $modelName.'/');
+                spl_autoload($className);
+            }
+        } else if ($breakClass[0] === 'login'){
+            if (file_exists(SITE_ROOT . 'module/' . $breakClass[0] . '/model/'. $modelName . '/' . $className . '.class.singleton.php')) {
+                // error_log("Entro login");
+                set_include_path('module/' . $breakClass[0] . '/model/' . $modelName.'/');
+                spl_autoload($className);
+            }
+        } else if (file_exists(SITE_ROOT . 'model/' . $className . '.class.singleton.php')){
             set_include_path(SITE_ROOT . 'model/');
             spl_autoload($className);
         }else if (file_exists(SITE_ROOT . 'model/' . $className . '.class.php')){
