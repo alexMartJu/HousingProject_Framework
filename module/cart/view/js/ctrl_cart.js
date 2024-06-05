22a27c633e61110b30a92d9344f040ce66eb7bca
alex <alex.mart.juan@gmail.com>
2024-06-05 10:16:13 +0200

    Modificación_JavascriptCart_Añadido_a_PromisesJs(main.js)

--- module/cart/view/js/ctrl_cart.js
+++ module/cart/view/js/ctrl_cart.js
@@ -1,51 +1,3 @@
-function addCart(element){
-    var id_housing = $(element).attr("id");
-    console.log("id_housing", id_housing);
-    var access_token = localStorage.getItem('access_token');
-    var refresh_token = localStorage.getItem('refresh_token');
-    if(access_token && refresh_token){
-        ajaxPromise(friendlyURL('?module=cart'), 'POST', 'JSON', {'access_token': access_token, 'id': id_housing, 'op':'add_update_Cart'})
-        .then(function(data) {
-            if (data === "insert" || data === "update") {
-                console.log("addCart --> insert/update");
-                updateItemsCart();
-            }
-        }).catch(function(error) {
-            console.log("Error ajaxPromise addCart", error);
-        });  
-    }else{
-        Swal.fire({
-            icon: 'error',
-            title: 'Restricted Cart Action',
-            text: 'You need to be logged in to buy something',
-            showConfirmButton: false,
-            timer: 2000
-        }).then(function() {
-            window.location.href = friendlyURL('?module=login');
-        });
-    }
-}
-
-function updateItemsCart() {
-    var access_token = localStorage.getItem('access_token');
-    var refresh_token = localStorage.getItem('refresh_token');
-    if(access_token && refresh_token){
-        ajaxPromise(friendlyURL('?module=cart'), 'POST', 'JSON', {'access_token': access_token, 'op':'updateItemsCart'})
-        .then(function(data) {
-            if(data.type === "numberItems") {
-                console.log("Entro updateItemsCart()", data.number_lines);
-                $('.cart-count').text(data.number_lines);
-            } else if(data.type === "error") {
-                console.log("Error: No se pudo obtener el número de líneas");
-            } else {
-                console.log("Error: Respuesta desconocida del servidor");
-            }
-        }).catch(function(error) {
-            console.log("Error ajaxPromise addCart", error);
-        });  
-    }
-}
-
 function paint_cart() {
     var access_token = localStorage.getItem('access_token');
     var refresh_token = localStorage.getItem('refresh_token');
@@ -203,5 +155,4 @@ function delete_line_Cart(){
 $(document).ready(function() {
     paint_cart();
     delete_line_Cart();
-    updateItemsCart()
 });
--- view/js/promises.js
+++ view/js/promises.js
@@ -169,8 +169,76 @@ function load_content() {
     }
 }
 
+function addCart(element){
+    var id_housing = $(element).attr("id");
+    console.log("id_housing", id_housing);
+    var access_token = localStorage.getItem('access_token');
+    var refresh_token = localStorage.getItem('refresh_token');
+    if(access_token && refresh_token){
+        ajaxPromise(friendlyURL('?module=cart'), 'POST', 'JSON', {'access_token': access_token, 'id': id_housing, 'op':'add_update_Cart'})
+        .then(function(data) {
+            console.log("addCart", data);
+            if (data === "insert" || data === "update") {
+                console.log("addCart --> insert/update");
+                updateItemsCart();
+            }else if (data === "exceeds_stock") {
+                Swal.fire({
+                    icon: 'warning',
+                    title: 'Stock Limit Exceeded',
+                    text: 'You have reached the maximum stock available for this item.',
+                    showConfirmButton: false,
+                    timer: 2000
+                });
+            } else if (data === "out_of_stock") {
+                Swal.fire({
+                    icon: 'warning',
+                    title: 'Out of Stock',
+                    text: 'This item is currently out of stock.',
+                    showConfirmButton: false,
+                    timer: 2000
+                });
+            }
+        }).catch(function(error) {
+            console.log("Error ajaxPromise addCart", error);
+        });  
+    }else{
+        Swal.fire({
+            icon: 'error',
+            title: 'Restricted Cart Action',
+            text: 'You need to be logged in to buy something',
+            showConfirmButton: false,
+            timer: 2000
+        }).then(function() {
+            window.location.href = friendlyURL('?module=login');
+        });
+    }
+}
+
+function updateItemsCart() {
+    var access_token = localStorage.getItem('access_token');
+    var refresh_token = localStorage.getItem('refresh_token');
+    if(access_token && refresh_token){
+        ajaxPromise(friendlyURL('?module=cart'), 'POST', 'JSON', {'access_token': access_token, 'op':'updateItemsCart'})
+        .then(function(data) {
+            if(data.type === "numberItems") {
+                console.log("Entro updateItemsCart()", data.number_lines);
+                $('.cart-count').text(data.number_lines);
+            } else if(data.type === "error") {
+                console.log("Error: No se pudo obtener el número de líneas");
+            } else {
+                console.log("Error: Respuesta desconocida del servidor");
+            }
+        }).catch(function(error) {
+            console.log("Error ajaxPromise addCart", error);
+        });  
+    }
+}
+
 $(document).ready(function() {
     load_menu();
     click_logout();
+    setTimeout(function() {
+        updateItemsCart();
+    }, 500);
     load_content()
 });
